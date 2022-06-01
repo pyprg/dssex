fixes calculation of loads

src/dssex/injections.py
@@ -53,33 +53,32 @@ def get_coefficients(x, y, dydx_0, dydx):
     x_sqr = x * x
     x_cub = x * x_sqr
     cm = np.array([
-        [   x_cub, x_sqr,  x],
-        [      0.,    0., 1.],
-        [3.*x_sqr,  2.*x, 1.]])
+        [   x_cub, x_sqr,  x],  # f(x1)
+        [      0.,    0., 1.],  # df(x0)/dx
+        [3.*x_sqr,  2.*x, 1.]]) # df(x1)/dx
     return solve(cm, np.array([y, dydx_0, dydx]))
 
-def calc_dpower_dvsqr(v_sqr, _2p):
-    """Calculates the derivative of exponential power function at v_sqr.
+def calc_dx(x, exp):
+    """Calculates the derivative of exponential power function at x.
     
     Parameters
     ----------
-    v_sqr: float
-        square of voltage magnitude
-    _2p: float
-        double of voltage exponent
+    x: float
+        magnitude
+    exp: float
+        exponent
     
     Returns
     -------
     float"""
-    p = _2p/2
-    return p * np.power(v_sqr, p-1)
+    return exp * np.power(x, exp-1)
 
-def get_polynomial_coefficients(ul, exp):
+def get_polynomial_coefficients(x1, exp):
     """Calculates coefficients of polynomials for interpolation.
     
     Parameters
     ----------
-    ul: float
+    x1: float
         upper limit of interpolation range
     exp: numpy.array
         float, exponents of function 'load over voltage'
@@ -89,8 +88,45 @@ def get_polynomial_coefficients(ul, exp):
     numpy.array
         float, shape(n,3)"""
     return np.vstack([
-        get_coefficients(ul, 1., 1., dp)
-        for dp in calc_dpower_dvsqr(ul, exp)])
+        get_coefficients(x1, y1, 1., dx) 
+        for y1, dx in zip(np.power(x1, exp), calc_dx(x1, exp))])
+
+def polynome(x1, exp, x):
+    """Function for checking the polynome.
+    
+    Parameters
+    ----------
+    x1: float
+        coordinate at which y1 = x1**exp
+    exp: float
+        exponent of original function
+    x: float
+        value to calculate y for"""
+    xsqr = x*x
+    coeffs = get_coefficients(
+        x1, 
+        x1**exp, 1., 
+        calc_dx(x1, exp))
+    return (coeffs[0] * xsqr * x) + (coeffs[1] * xsqr) + (coeffs[2] * x)
+
+def interpolate(x1, exp, x):
+    """Function for checking the interpolation concept.
+    the function x**exp shall be interpolated near x~0 that y->0 for x=0.
+    The function is interpolated with a polynome of third order.
+    
+    Parameters
+    ----------
+    x1: float
+        upper limit of interpolation, above the limit y is according to 
+        the original function, below is interpolated
+    exp: float
+        exponent of original function
+    x: float
+        value to calculate y for"""
+    if x < x1:
+        return polynome(x1, exp, x)
+    else:
+        return x**exp
 
 def add_interpol_coeff_to_injections(injections, vminsqr):
     """Adds polynomial coefficients to injections for linear interpolation

src/dssex/lab.py
@@ -51,19 +51,19 @@ model_devices = [
         )]
 
 model = make_model(model_devices)
-get_injected_power = get_injected_power_fn(model.injections)
+get_injected_power = get_injected_power_fn(model.injections, loadcurve='square')
 pq_factors = np.ones((len(model.injections), 2))
 if len(model.errormessages):
     print(model.errormessages)
 else:
-    success, V = cpfnum(1e-10, 20, model)
+    success, V = cpfsymb(1e-10, 20, model)
     print('SUCCESS' if success else '_F_A_I_L_E_D_')
     Ires = get_residual_current_fn(model, get_injected_power)(V)
-    print('Ires: ', Ires)
-    print('V: ', pd.DataFrame(V, columns=['V']))
+    print('\nIres:\n', Ires)
+    print('\nV:\n', pd.DataFrame(V, columns=['V']))
     res = get_results(model, get_injected_power, model.branchtaps.position, V)  
-    print(res['injections'])
-    print(res['branches'])  
+    print('\ninjections:\n', res['injections'])
+    print('\nbranches:\n', res['branches'])  
 #%%
 from dnadb import egrid_frames
 from dnadb.ifegrid import decorate_injection_results, decorate_branch_results
@@ -80,44 +80,44 @@ import casadi
 #path = r"K:\Siemens\Power\Temp\DSSE\Subsystem_142423"
 
 
-fv = .8 # voltage
-fl = 2 # loads
-loadcurve = 'sq' # 'original' | 'interpolated' | 'square'
+fv = 1. # voltage
+fl = 2.9 # loads
+loadcurve = 'square' # 'original' | 'interpolated' | 'square'
 powerflowfn = cpfsymb # cpfnum | cpfsymb
 
 path = r"D:\eus1_loop"
 frames = egrid_frames(_Y_LO_ABS_MAX, path)
 model = model_from_frames(frames)
 pq_factors = fl * np.ones((len(model.injections), 2))
-calc_Inode_inj = build_injected_current_fn(model, pq_factors, loadcurve)
 
 
 fn_Iresidual = build_residual_fn(model, pq_factors, loadcurve)
+# num
+get_injected_power = get_injected_power_fn(
+    model.injections, pq_factors=pq_factors, loadcurve=loadcurve)
 
 
 #
 # check current calculaiton function
 #
 
-fVprobe = 1.
+# fVprobe = 0.4 # 0.8944271909999159
+# Vprobe = fVprobe * (
+#         np.array([1.+0j]*model.shape_of_Y[0], dtype=np.complex128))
+# # symb
+# calc_Inode_inj = build_injected_current_fn(model, pq_factors, loadcurve)
+# Vprobe_ri = np.concatenate([np.real(Vprobe), np.imag(Vprobe)]).reshape(-1)
+# Inode_inj_ri = np.array(calc_Inode_inj(Vprobe_ri))
+# Inode_inj = np.hstack(np.split(Inode_inj_ri, 2)).view(dtype=np.complex128)
+# print('\nInode_inj:\n', Inode_inj)
+# # util
+# Inode_inj2 = get_injected_current_per_node(
+#     get_injected_power, model, Vprobe.reshape(-1, 1))
+# print('\nInode_inj2:\n', Inode_inj2)
+
 Vslack = fv * model.slacks.V
 values_of_params = casadi.horzcat(
     np.real(Vslack), np.imag(Vslack), model.branchtaps.position.copy())
-Vprobe = fVprobe * (
-        np.array([1.+0j]*model.shape_of_Y[0], dtype=np.complex128))
-# symb
-Vprobe_ri = np.concatenate([np.real(Vprobe), np.imag(Vprobe)]).reshape(-1)
-Inode_inj_ri = np.array(calc_Inode_inj(Vprobe_ri))
-Inode_inj = np.hstack(np.split(Inode_inj_ri, 2)).view(dtype=np.complex128)
-print('\nInode_inj:\n', Inode_inj)
-# num
-get_injected_power = get_injected_power_fn(
-    model.injections, pq_factors=pq_factors, loadcurve=loadcurve)
-# util
-Inode_inj2 = get_injected_current_per_node(get_injected_power, model, Vprobe.reshape(-1, 1))
-print('\nInode_inj2:\n', Inode_inj2)
-
-#%%
 if len(model.errormessages):
     print(model.errormessages)
 else:

src/dssex/pfcnum.py
@@ -315,12 +315,10 @@ def _get_interpolated_injected_power_fn(vminsqr, injections, pq_factors=None):
         Pres[~interpolate] *= np.power(Vsqr_orig, Exp_v_p_half[~interpolate])
         Qres[~interpolate] *= np.power(Vsqr_orig, Exp_v_q_half[~interpolate])
         # polynomial interpolated
-        Vsqr_inter = Vinj_abs_sqr2[interpolate]
+        Vsqr_inter = Vinj_abs_sqr2[interpolate].reshape(-1, 1)
         cinterpolate = coeffs[interpolate]
         V_abs = np.power(Vsqr_inter, .5)
-        V321 = (
-            np.hstack([Vsqr_inter * V_abs, Vsqr_inter, V_abs])
-            .reshape(-1, 3))
+        V321 = np.hstack([Vsqr_inter * V_abs, Vsqr_inter, V_abs])
         Pres[interpolate] *= np.sum(V321 * cinterpolate[:, :3], axis=1)
         Qres[interpolate] *= np.sum(V321 * cinterpolate[:, 3:], axis=1)
         return Pres, Qres

src/dssex/util.py
@@ -177,8 +177,7 @@ def get_injected_current_per_node(calculate_injected_power, model, Vnode):
     Sinj = (
         np.hstack([Pinj.reshape(-1, 1), Qinj.reshape(-1, 1)])
         .view(dtype=np.complex128))
-    # injected current is negative for positve power
-    return -np.conjugate((model.mnodeinj @ Sinj) / Vnode)
+    return np.conjugate((model.mnodeinj @ Sinj) / Vnode)
 
 def get_injection_results(calculate_injected_power, model, Vnode):
     """Returns active and reactive power in pu for given node voltages.
@@ -399,7 +398,7 @@ def get_residual_current(model, get_injected_power, Y, Vnode):
     V_ =  Vnode.reshape(-1, 1)
     Inode = Y @ V_
     Iinj = get_injected_current_per_node(get_injected_power, model, V_)
-    return (Inode - Iinj).reshape(-1)
+    return (Inode + Iinj).reshape(-1)
 
 def get_residual_current_fn(model, get_injected_power, tappositions=None):
     """Parameterizes function get_residual_current.
@@ -477,7 +476,7 @@ def get_residual_current2(model, get_injected_power, Vslack, Y, Vnode):
     Iinj = (
         get_injected_current_per_node(get_injected_power, model, V_)
         [count_of_slacks:])
-    return (Inode - Iinj).reshape(-1)
+    return (Inode + Iinj).reshape(-1)
 
 def get_residual_current_fn2(
         model, get_injected_power, Vslack=None, tappositions=None):