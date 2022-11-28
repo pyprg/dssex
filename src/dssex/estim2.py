fixes branch-tap-factor processing, fixes injected current calculation

src/dssex/estim2.py
@@ -81,8 +81,8 @@ def create_V_symbols(count_of_nodes):
     return casadi.horzcat(Vre, Vim, Vre.constpow(2)+Vim.constpow(2))
 
 def get_tap_factors(branchtaps, position_syms):
-    """Creates expressions for longitudinal and transversal factors
-    of branches.
+    """Creates expressions for off-diagonal factors of branches.
+    Diagonal factors are just the square of the off-diagonal factors.
 
     Parameters
     ----------
@@ -94,20 +94,17 @@ def get_tap_factors(branchtaps, position_syms):
 
     Returns
     -------
-    casadi.SX
-        * [:,0] longitudinal factors
-        * [:,1] transversal factors"""
+    casadi.SX"""
     if position_syms.size1():
-        # longitudinal factor
-        flo = (1
+        return (
+            1
             - casadi.SX(branchtaps.Vstep)
               * (position_syms - branchtaps.positionneutral))
     else:
-        flo = casadi.SX(0, 1)
-    return casadi.horzcat(flo, casadi.constpow(flo, 2))
+        return casadi.SX(0, 1)
 
 def _mult_gb_by_tapfactors(
-        gb_mn_mm, flo_ftr, index_of_term, index_of_other_term):
+        gb_mn_tot, foffd, index_of_term, index_of_other_term):
     """Multiplies conductance and susceptance of branch terminals in order
     to consider positions of taps.
 
@@ -115,14 +112,12 @@ def _mult_gb_by_tapfactors(
     ----------
     gb_mn_mm: casadi.SX
         matrix of 4 column vectors, one row for each branch terminal
-        gb_mn_mm[:,0] - g_mn
-        gb_mn_mm[:,1] - b_mn
-        gb_mn_mm[:,2] - g_mm
-        gb_mn_mm[:,3] - b_mm
-    flo_ftr: casadi.SX
-        matrix of 2 column vectors, one row for each tap position
-        flo_ftr[:,0] - longitudinal factor
-        flo_ftr[:,1] - transversal factor
+        gb_mn_tot[:,0] - g_mn
+        gb_mn_tot[:,1] - b_mn
+        gb_mn_tot[:,2] - g_tot
+        gb_mn_tot[:,3] - b_tot
+    foffd: casadi.SX
+        vector - off-diagonal factor
     index_of_term: array_like
         taps -> index of terminal
     index_of_other_term: array_like
@@ -131,19 +126,18 @@ def _mult_gb_by_tapfactors(
     Returns
     -------
     casadi.SX
-        gb_mn_mm[:,0] - g_mn
-        gb_mn_mm[:,1] - b_mn
-        gb_mn_mm[:,2] - g_mm
-        gb_mn_mm[:,3] - b_mm"""
-    assert flo_ftr.size1(), "factors required"
+        gb_mn_tot[:,0] - g_mn
+        gb_mn_tot[:,1] - b_mn
+        gb_mn_tot[:,2] - g_tot
+        gb_mn_tot[:,3] - b_tot"""
+    assert foffd.size1(), "factors required"
     assert len(index_of_term), "indices of terminals required"
     assert len(index_of_other_term), "indices of other terminals required"
-    flo = flo_ftr[:, 0]
-    gb_mn_mm[index_of_term, :2] *= flo
-    gb_mn_mm[index_of_other_term, :2] *= flo
+    gb_mn_tot[index_of_term, :2] *= foffd
+    gb_mn_tot[index_of_other_term, :2] *= foffd
     # transversal factor
-    gb_mn_mm[index_of_term, 2:] *= flo_ftr[:, 1]
-    return gb_mn_mm
+    gb_mn_tot[index_of_term, 2:] *= (foffd*foffd)
+    return gb_mn_tot
 
 def _create_gb_expressions(terms):
     g_mn = casadi.SX(terms.g_lo)
@@ -700,7 +694,8 @@ def make_get_scaling_data(model, count_of_steps=1):
                 column vector, symbols for constants of scaling factors
             values_of_consts: casadi.DM
                 column vector, values for consts"""
-    assert 0 < count_of_steps, "count_of_steps must be an int greater than 0"
+    assert 0 < count_of_steps, \
+        "count_of_steps must be an integer greater than 0"
     factors, injection_factors = get_factors(model, count_of_steps)
     return partial(
         get_scaling_data,
@@ -726,13 +721,15 @@ def _calculate_injected_power_n(Vinj_abs_sqr, Exp_v, PQ):
         active or reactive power or both, dimension must match dimension of 
         Exp_v
     
-    Para"""
+    Returns
+    -------
+    numpy.array (shape PQ.shape)"""
     assert Exp_v.shape == PQ.shape, \
         f'shapes of Exp_v and PQ must match ' \
         f'but do not {Exp_v.shape}!={PQ.shape}'
     return (
         (np.power(Vinj_abs_sqr.reshape(-1, 1), Exp_v/2) * PQ)
-        .reshape(Exp_v.shape))
+        .reshape(PQ.shape))
 
 def _interpolate_injected_power_n(Vinj_abs_sqr, Exp_v, PQ, vminsqr):
     """Interpolates values of injected power.
@@ -1070,8 +1067,14 @@ def _current_into_injection(
     cpq = calculate_cubic_coefficients(vminsqr, Exp_v)
     V_321 = casadi.vertcat(
         (Vinj_abs_sqr * Vinj_abs).T, Vinj_abs_sqr.T, Vinj_abs.T)
-    Pip = (casadi.SX(cpq[:,:,0]) @ V_321) * PQscaled[:,0]
-    Qip = (casadi.SX(cpq[:,:,1]) @ V_321) * PQscaled[:,1]
+    Pip_ = casadi.vcat(
+        [(casadi.SX(cpq[row_index,:,0].reshape(1,-1)) @ V_321[:,row_index])
+         for row_index in range(V_321.size2())])
+    Qip_ = casadi.vcat(
+        [(casadi.SX(cpq[row_index,:,1].reshape(1,-1)) @ V_321[:,row_index])
+         for row_index in range(V_321.size2())])
+    Pip = Pip_ * PQscaled[:,0]
+    Qip = Qip_ * PQscaled[:,1]
     # current according to given load curve
     I_orig = _calculate_injected_current(
         Vinj[:,:2], Vinj_abs_sqr, Exp_v, PQscaled)
@@ -1120,17 +1123,19 @@ def _create_gb_mn_tot(branchterminals, branchtaps, position_syms):
         * [:,1] b_mn, mutual susceptance
         * [:,2] g_tot, self conductance + mutual conductance
         * [:,3] b_tot, self susceptance + mutual susceptance"""
-    gb_mn_mm = _create_gb_expressions(branchterminals)
+    # greate gb_mn_mm
+    gb_mn_tot = _create_gb_expressions(branchterminals)
+    # create gb_mn_tot
+    gb_mn_tot[:,2] += gb_mn_tot[:,0]
+    gb_mn_tot[:,3] += gb_mn_tot[:,1]
     if position_syms.size1():
-        flo_ftr = get_tap_factors(branchtaps, position_syms)
-        gb_mn_mm = _mult_gb_by_tapfactors(
-            gb_mn_mm,
-            flo_ftr,
+        foffd = get_tap_factors(branchtaps, position_syms)
+        return _mult_gb_by_tapfactors(
+            gb_mn_tot,
+            foffd,
             branchtaps.index_of_term,
             branchtaps.index_of_other_term)
-    gb_mn_mm[:,2] += gb_mn_mm[:,0]
-    gb_mn_mm[:,3] += gb_mn_mm[:,1]
-    return gb_mn_mm
+    return gb_mn_tot
 
 def create_v_symbols_gb_expressions(model):
     """Creates symbols for node and slack voltages, tappositions,

src/dssex/pfcnum.py
@@ -39,7 +39,7 @@ _VMINSQR = 0.8**2
 _zeros = np.zeros((0, 1), dtype=np.longdouble)
 _power_props = itemgetter('P10', 'Q10', 'Exp_v_p', 'Exp_v_q')
 
-def get_gb_terms(terms, flo, ftr):
+def get_gb_terms(terms, foffd):
     """Multiplies conductance/susceptance of branches with factors retrieved
     from tap positions.
     
@@ -47,35 +47,38 @@ def get_gb_terms(terms, flo, ftr):
     ----------
     terms: pandas.DataFrame
     
-    flo: pandas.Series, float
-        factor for longitudinal admittance
-    ftr: pandas.Series, float
-        factor transversal admittance
+    foffd: pandas.Series, float
+        tap-factor for off-diagonal admittance y_mn
     
     Returns
     -------
     tuple
         * numpy.array, float, g_lo
-        * numpy.array, float, g_tr
+        * numpy.array, float, g_tot
         * numpy.array, float, b_lo
-        * numpy.array, float, b_tr"""
+        * numpy.array, float, b_tot"""
     terms_with_taps = terms[terms.index_of_taps.notna()]
     idx_of_tap = terms_with_taps.index_of_taps
-    g_tr = terms.g_tr_half.to_numpy()
-    b_tr = terms.b_tr_half.to_numpy()
-    g_tr[terms_with_taps.index] *= ftr[idx_of_tap]   
-    b_tr[terms_with_taps.index] *= ftr[idx_of_tap]    
     g_lo = terms.g_lo.to_numpy()
     b_lo = terms.b_lo.to_numpy()
-    g_lo[terms_with_taps.index] *= flo[idx_of_tap]
-    b_lo[terms_with_taps.index] *= flo[idx_of_tap]
+    g_tot = terms.g_tr_half.to_numpy() + g_lo
+    b_tot = terms.b_tr_half.to_numpy() + b_lo
+    foffd_of_tap = foffd[idx_of_tap]
+    fdiag_of_tap = foffd_of_tap * foffd_of_tap
+    g_tot[terms_with_taps.index] *= fdiag_of_tap   
+    b_tot[terms_with_taps.index] *= fdiag_of_tap    
+    g_lo = terms.g_lo.to_numpy()
+    b_lo = terms.b_lo.to_numpy()
+    g_lo[terms_with_taps.index] *= foffd_of_tap
+    b_lo[terms_with_taps.index] *= foffd_of_tap
     terms_with_other_taps = terms[terms.index_of_other_taps.notna()]
     idx_of_other_tap = terms_with_other_taps.index_of_other_taps
-    g_lo[terms_with_other_taps.index] *= flo[idx_of_other_tap]
-    b_lo[terms_with_other_taps.index] *= flo[idx_of_other_tap]
-    return g_lo, g_tr, b_lo, b_tr
+    foffd_of_other_tap = foffd[idx_of_other_tap]
+    g_lo[terms_with_other_taps.index] *= foffd_of_other_tap
+    b_lo[terms_with_other_taps.index] *= foffd_of_other_tap
+    return g_lo, g_tot, b_lo, b_tot
 
-def create_gb(terms, count_of_nodes, flo, ftr):
+def create_gb(terms, count_of_nodes, foffd):
     """Generates a conductance-susceptance matrix of branches equivalent to
     branch-admittance matrix. M[n,n] of slack nodes is set to 1, other
     values of slack nodes are zero.
@@ -101,9 +104,9 @@ def create_gb(terms, count_of_nodes, flo, ftr):
     row = np.concatenate([index_of_node, index_of_node])
     col = np.concatenate([index_of_node, index_of_other_node])
     rowcol = row, col
-    g_lo, g_tr, b_lo, b_tr = get_gb_terms(terms, flo, ftr)
-    gvals = np.concatenate([(g_tr + g_lo), -g_lo])
-    bvals = np.concatenate([(b_tr + b_lo), -b_lo])
+    g_lo, g_tot, b_lo, b_tot = get_gb_terms(terms, foffd)
+    gvals = np.concatenate([g_tot, -g_lo])
+    bvals = np.concatenate([b_tot, -b_lo])
     shape = count_of_nodes, count_of_nodes
     g = coo_matrix((gvals, rowcol), shape=shape, dtype=float)
     b = coo_matrix((bvals, rowcol), shape=shape, dtype=float)
@@ -125,11 +128,11 @@ def create_gb_matrix(model, pos):
     Returns
     -------
     scipy   sparse   matrix"""
-    flo, ftr = get_tap_factors(model.branchtaps, pos)
+    foffd = get_tap_factors(model.branchtaps, pos)
     count_of_nodes = model.shape_of_Y[0]
     terms = (
         model.branchterminals[~model.branchterminals.is_bridge].reset_index())
-    G, B = create_gb(terms, count_of_nodes, flo, ftr)
+    G, B = create_gb(terms, count_of_nodes, foffd)
     count_of_slacks = model.count_of_slacks
     diag = diags(
         [1.] * count_of_slacks, 

src/dssex/util.py
@@ -28,8 +28,8 @@ from scipy.sparse import coo_matrix, diags, vstack
 from functools import partial
 
 def get_tap_factors(branchtaps, pos):
-    """Creates factors for tap positions, values for longitudinal and
-    transversal factors of branches.
+    """Creates factors for tap positions, values for off-diagonal and
+    diagonal factors of branches.
     
     Parameters
     ----------
@@ -42,14 +42,14 @@ def get_tap_factors(branchtaps, pos):
     Returns
     -------
     tuple
-        * numpy array, float, longitudinal factors
-        * numpy array, float, transversal factors"""
+        * numpy array, float, off-diagonal factors
+        * numpy array, float, diagonal factors"""
     # factor longitudinal
-    flo = (1 - branchtaps.Vstep.to_numpy() * (
+    foffd = (1 - branchtaps.Vstep.to_numpy() * (
         pos - branchtaps.positionneutral.to_numpy()))
-    return flo, np.power(flo, 2)
+    return foffd
 
-def get_y_terms(terms, flo, ftr):
+def get_y_terms(terms, foffd):
     """Multiplies admittances of branches with factors retrieved
     from tap positions.
     
@@ -57,28 +57,28 @@ def get_y_terms(terms, flo, ftr):
     ----------
     terms: pandas.DataFrame
     
-    flo: pandas.Series, float
-        factor for longitudinal admittance
-    ftr: pandas.Series, float
-        factor transversal admittance
+    foffd: pandas.Series, float
+        factor for off-diagonal admittance -y_mn
     
     Returns
     -------
     tuple
-        * numpy.array, complex, y_tr, transversal admittance
-        * numpy.array, complex, y_lo, longitudinal admittance"""
+        * numpy.array, complex, y_lo, longitudinal admittance, y_mn
+        * numpy.array, complex, y_tot, diagonal admittance y_mm"""
     terms_with_taps = terms[terms.index_of_taps.notna()]
     idx_of_tap = terms_with_taps.index_of_taps
     y_tr = terms.y_tr_half.to_numpy()
-    y_tr[terms_with_taps.index] *= ftr[idx_of_tap]   
     y_lo = terms.y_lo.to_numpy()
-    y_lo[terms_with_taps.index] *= flo[idx_of_tap]
+    y_tot = y_tr + y_lo
+    foffd_of_tap = foffd[idx_of_tap] 
+    y_lo[terms_with_taps.index] *= foffd_of_tap   
+    y_tot[terms_with_taps.index] *= (foffd_of_tap*foffd_of_tap)
     terms_with_other_taps = terms[terms.index_of_other_taps.notna()]
     idx_of_other_tap = terms_with_other_taps.index_of_other_taps
-    y_lo[terms_with_other_taps.index] *= flo[idx_of_other_tap]
-    return y_tr, y_lo
+    y_lo[terms_with_other_taps.index] *= foffd[idx_of_other_tap]
+    return y_lo, y_tot
 
-def create_y(terms, count_of_nodes, flo, ftr):
+def create_y(terms, count_of_nodes, foffd):
     """Generates the branch-admittance matrix. 
     
     Parameters
@@ -87,10 +87,9 @@ def create_y(terms, count_of_nodes, flo, ftr):
     
     count_of_nodes: int
         number of power flow calculation nodes
-    flo: array_like
-        double, longitudinal taps factor, sparse for terminals with taps
-    ftr: array_like
-        transversal taps factor, sparse for terminals with taps
+    foffd: array_like
+        double, off-diagonal factor for y_mn and y_nm admittances, 
+        sparse for terminals with taps
     
     Returns
     -------
@@ -101,8 +100,8 @@ def create_y(terms, count_of_nodes, flo, ftr):
     row = np.concatenate([index_of_node, index_of_node])
     col = np.concatenate([index_of_node, index_of_other_node])
     rowcol = row, col
-    y_lo, y_tr = get_y_terms(terms, flo, ftr)
-    yvals = np.concatenate([(y_tr + y_lo), -y_lo])
+    y_lo, y_tot = get_y_terms(terms, foffd)
+    yvals = np.concatenate([y_tot, -y_lo])
     shape = count_of_nodes, count_of_nodes
     return coo_matrix((yvals, rowcol), shape=shape, dtype=np.complex128)
 
@@ -122,11 +121,11 @@ def create_y_matrix(model, pos):
     Returns
     -------
     scipy.sparse.matrix"""
-    flo, ftr = get_tap_factors(model.branchtaps, pos)
+    foffd = get_tap_factors(model.branchtaps, pos)
     count_of_nodes = model.shape_of_Y[0]
     terms = (
         model.branchterminals[~model.branchterminals.is_bridge].reset_index())
-    Y = create_y(terms, count_of_nodes, flo, ftr)
+    Y = create_y(terms, count_of_nodes, foffd)
     count_of_slacks = model.count_of_slacks
     diag = diags(
         [1.+0.j] * count_of_slacks, 
@@ -237,28 +236,28 @@ def get_crossrefs(terms, count_of_nodes):
         dtype=np.int8).tocsc()
     return mtermnode, mtermothernode
 
-def get_branch_admittance_matrices(y_tr, y_lo, term_is_at_A):
+def get_branch_admittance_matrices(y_lo, y_tot, term_is_at_A):
     """Creates a 2x2 branch-admittance matrix for each branch.
     
     Parameters
     ----------
-    y_tr: numpy.array, complex
-        transversal admittance according to pi-equivalent circuit, per branch
     y_lo: numpy.array, complex
-        longitudinal admittance according to pi-equivalent circuit, per branch
+        y_mn admittance, per branch
+    y_tot: numpy.array, complex
+        (y_mn + y_mm) admittance, per branch
     term_is_at_A: numpy.array, bool, index of terminal
-        True if terminal with is at side A of a branch
+        True if terminal is at side A of a branch
         
     Returns
     -------
     numpy.darray, complex, shape=(:, 2, 2)"""
-    y_tr_A = y_tr[term_is_at_A]
-    y_tr_B = y_tr[~term_is_at_A]
+    y_tot_A = y_tot[term_is_at_A]
+    y_tot_B = y_tot[~term_is_at_A]
     y_lo_AB = y_lo[term_is_at_A]
-    y_11 = y_tr_A + y_lo_AB
+    y_11 = y_tot_A
     y_12 = -y_lo_AB
     y_21 = -y_lo_AB
-    y_22 = y_tr_B + y_lo_AB
+    y_22 = y_tot_B
     return (np.hstack([
          y_11.reshape(-1, 1), 
          y_12.reshape(-1, 1), 
@@ -283,9 +282,9 @@ def get_y_branches(model, terms, term_is_at_A, pos):
     Returns
     -------
     numpy.array, complex, shape=(:, 2, 2)"""
-    flo, ftr = get_tap_factors(model.branchtaps, pos)
-    y_tr, y_lo = get_y_terms(terms, flo, ftr)
-    return get_branch_admittance_matrices(y_tr, y_lo, term_is_at_A)
+    foffd = get_tap_factors(model.branchtaps, pos)
+    y_lo, y_tot = get_y_terms(terms, foffd)
+    return get_branch_admittance_matrices(y_lo, y_tot, term_is_at_A)
 
 def get_v_branches(terms, voltages):
     """Creates a voltage vector 2x1 per branch.
@@ -469,7 +468,6 @@ def max_ri(cx_array):
 
 def create_y_matrix2(model, pos):
     """Generates admittance matrix of branches without rows for slacks. 
-    Should return a symmetric matrix.
     
     Parameters
     ----------
@@ -481,11 +479,11 @@ def create_y_matrix2(model, pos):
     Returns
     -------
     scipy.sparse.matrix"""
-    flo, ftr = get_tap_factors(model.branchtaps, pos)
+    foffd = get_tap_factors(model.branchtaps, pos)
     count_of_nodes = model.shape_of_Y[0]
     terms = (
         model.branchterminals[~model.branchterminals.is_bridge].reset_index())
-    Y = create_y(terms, count_of_nodes, flo, ftr)
+    Y = create_y(terms, count_of_nodes, foffd)
     count_of_slacks = model.count_of_slacks
     return Y.tocsc()[count_of_slacks:, :]
 